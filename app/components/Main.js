6a3498368df153ff2fea01418d4fb5543612a799
Anurag Singh Bisht <anurag.bisht@imaginea.com>
2017-05-15 18:49:33 +0530

    Update redirection logic for dashboard

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
 import App from './App'
 import BooksPage from './BooksPage'
 import RequestAuthentication from './RequestAuthentication'
+import { connect } from 'react-redux'
 
-const Main = ({ authenticated }) => (
+const Main = ({ checked }) => (
+  checked &&
   <Switch>
     <Route exact path='/' component={BooksPage} />
     <Route path='/books' component={RequestAuthentication(App)} />
   </Switch>
 )
 
-export default Main
+const mapStateToProps = (state) => {
+  return {
+    checked: state.session.checked
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Main))

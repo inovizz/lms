5be8cce4f9c6ba7f3cb62e117b4de66481ef0324
Chandan Kumar <chandan.gupta@gmail.com>
2017-05-25 15:15:48 +0530

    Fix bug in build script (#63)
    The build steps are executed from scripts directory since the
    build script resides there. Changed "cd" command to move up one
    directory.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,16 @@
 # Used by Jenkins to deploy code to EC2 instance.
 # After deploying a new contract, update the contract address below.
 
-cd lms
-sed -i s/0x[0-9a-z]*/0xa1f1405b1aae0bf18d4515e140a62186b8ab0f3c/ app/config.js
+# Enable debug mode
+set -x
+
+cd ..
+sed -i s/0x[0-9a-z]*/0xa1f1405b1aae0bf18d4515e140a62186b8ab0f3c/      app/config.js
 sed -i s/localhost/ec2-35-164-104-24.us-west-2.compute.amazonaws.com/ app/web3.js
 sed -i s/localhost/ec2-35-164-104-24.us-west-2.compute.amazonaws.com/ server/routes.js
 sed -i s/localhost/ec2-35-164-104-24.us-west-2.compute.amazonaws.com/ index.js
 sed -i s/localhost/ec2-35-164-104-24.us-west-2.compute.amazonaws.com/ truffle.js
 npm install && npm run build
+
+# Disable debug mode
+set +x

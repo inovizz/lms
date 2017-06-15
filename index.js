58832bf24f55152dae5413d27bd0f2ae7b1e9940
geekysuman <suman.sarkar@imaginea.com>
2017-06-15 12:42:57 +0530

    Create nodejs back-end server and api

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routing
+
+const api = require('./server/routes')(app, express);
+app.use('/api',api);
+
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname,'dist','index.html'));
 });
 
-const api = require('./server/routes')(app, express);
-app.use('/api',api);
 
 // Start Server
 app.listen(config.port, 'localhost', function(err) {
